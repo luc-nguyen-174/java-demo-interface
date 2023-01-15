9f5464798863e25d3fca3d9055502f558ff1e6dc
luc-nguyen'174 <louiskingvn@gmail.com>
2023-01-15 23:51:58 +0700

    first commit

---
+++ src/com/codegym/ShapeTest.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red",false);
+        System.out.println(shape);
+
+        Circle circle = new Circle();
+        System.out.println("\n"+circle);
+    }
+}
